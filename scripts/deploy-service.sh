fe18890bbb562b55c100d977aa38177ca72a876a
David Corino <davidcorino@hotmail.com>
2020-05-24 20:56:58 +0000

    Updating Script

--- scripts/deploy-service.sh
+++ scripts/deploy-service.sh
@@ -3,4 +3,4 @@
 SERVICE_FILE=$1
 NAMESPACE=$2
 
-docker stack deploy -c ../deploy_files/$SERVICE_FILE.yaml $NAMESPACE
+docker stack deploy -c /root/.jenkins/workspace/deploy_service/deploy_files/$SERVICE_FILE.yaml $NAMESPACE
