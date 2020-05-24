966cdc5c67679c0fc8f1b195909c49017d7b7081
David Corino <davidcorino@hotmail.com>
2020-05-24 19:06:33 +0000

    Updating Script

--- scripts/build-and-deploy-automation.sh
+++ scripts/build-and-deploy-automation.sh
@@ -39,18 +39,35 @@ docker service scale $SERVICE_NAME=$REPLICAS
 echo ""
 }
 
+view_logs() {
+
+echo "Your Service name is: "
+echo ""
+docker service ls | awk '{print $2}' | grep -v NAME
+
+echo ""
+read -p "Enter Service Name:" SERVICE_NAME
+echo ""
+
+docker service logs $SERVICE_NAME
+
+echo ""
+}
+
 while [ option != 0 ] ; do
 
         echo "1. Build Image"
         echo "2. Deploy service"
         echo "3. Scale Service"
-        echo "4. Exit"
+        echo "4. View Logs"
+	echo "5. Exit"
 	read option
  	case "${option}" in 
 	        1) build_image ;;
                 2) deploy_service ;;
                 3) scale_service ;;
-                4) exit 0;;
+                4) view_logs ;;
+		5) exit 0;;
 		*)
 			echo "Invalid Option"
 	esac
--- scripts/build-and-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t haproxy-challenge:1.0 .
-docker stack deploy -c docker-stack.yaml challenge
